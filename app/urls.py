533c8d62d7a76d5536845a9d4f0b07307d89144f
Nishant <sapkota.nishant@wmich.edu>
2020-05-09 14:43:40 -0400

    Frontend created

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from app.views import homepage, DisplayList, index
+from app.views import homepage, DisplayList
 urlpatterns = [
     path('', homepage.as_view(), name = 'index'),
     path('show/', DisplayList.as_view(), name = 'list'),
-    path('again/',index, name = 'yes' )
 ]
--- app/views.py
+++ app/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView, ListView
 from .models import ToDoList
-from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 # Create your views here.
 
 class homepage(TemplateView):
-    template_name = 'index.html'
+    template_name = 'homepage.html'
 
 class DisplayList(ListView):
     context_object_name = 'tasks'
     model = ToDoList
-    template_name = 'lists.html'
-
-
-def index(request):
-    return reverse_lazy('index')
+    template_name = 'Done Tasks.html'
