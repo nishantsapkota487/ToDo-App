a43c02c0efa25ed7bea5473ce717adb857dfa0bf
Nishant <sapkota.nishant@wmich.edu>
2020-05-09 17:52:39 -0400

    Fronend final created

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from app.views import homepage, DisplayList
+from app.views import  DisplayList
 urlpatterns = [
-    path('', homepage.as_view(), name = 'index'),
-    path('show/', DisplayList.as_view(), name = 'list'),
+    path('', DisplayList.as_view(), name = 'index'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,10 +4,7 @@ from .models import ToDoList
 from django.http import HttpResponseRedirect
 # Create your views here.
 
-class homepage(TemplateView):
-    template_name = 'homepage.html'
-
 class DisplayList(ListView):
     context_object_name = 'tasks'
     model = ToDoList
-    template_name = 'Done Tasks.html'
+    template_name = 'homepage.html'
